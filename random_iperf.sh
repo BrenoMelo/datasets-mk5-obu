36da756fcfb416f20e86204b7b3a61415c09e394
BrenoMelo <breno_Fabricio23@hotmail.com>
2024-02-23 15:24:51 +0000

    random_iperf.sh

--- random_iperf.sh
+++ random_iperf.sh
@@ -6,9 +6,8 @@ do
     rand_run=$(( $RANDOM % 26 + 5));
     rand_stop=$(( $RANDOM % 16 + 5))
 
-	echo "Iperf application will be running for $rand_run seconds. Next run will be in $rand_stop seconds.";
+    echo "Iperf application will be running for $rand_run seconds. Next run will be in $rand_stop seconds.";
     sudo iperf --client=192.168.234.1 -t $rand_run --tradeoff --interval=1 --format=m --tos=MK2_PRIO_4
-        #timeout $rand_run sudo ./runtest_iperf_tx.sh
 
     if [ $((COUNTER)) -eq 199 ]; then
         exit 1
